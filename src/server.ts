ae5c43acdc094a37fc9a55f5062d203a95ddc2b8
gui-adams <guilherme.adams@live.com>
2024-08-07 17:59:55 -0300

    banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors';
+import cors from 'cors';
 
 import { router } from './routers'
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use(router);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
