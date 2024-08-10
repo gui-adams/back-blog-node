be48b45a2df07a11820cf1502c8705d6ed5fbd56
gui-adams <guilherme.adams@live.com>
2024-08-09 21:15:32 -0300

    file img

--- src/controllers/post/CreatePostController.ts
+++ src/controllers/post/CreatePostController.ts
@@ -3,15 +3,22 @@ import { CreatePostService } from '../../services/post/CreatePostService';
 
 class CreatePostController {
     async handle(req: Request, res: Response) {
-        const { title, description, conteudo, banner, draft, published, category_id } = req.body;
+        const { title, description, conteudo, draft, published, category_id } = req.body;
 
         // Obtendo o ID do usuário autenticado
         const author_id = req.user_id;
 
         // Verifique se todos os campos obrigatórios estão presentes
-        if (!title || !conteudo || !banner || !category_id || !author_id) {
+        if (!title || !conteudo || !category_id || !author_id) {
             return res.status(400).json({ error: "Missing required fields" });
         }
+
+        // Verifique se o arquivo foi enviado
+        if (!req.file) {
+            return res.status(400).json({ error: "File upload failed" });
+        }
+
+        const { originalname, filename: banner } = req.file;
 
         const createPostService = new CreatePostService();
 
@@ -20,9 +27,9 @@ class CreatePostController {
                 title,
                 description,
                 conteudo,
-                banner,
-                draft,
-                published,
+                banner,  // O banner é obrigatório e é o nome do arquivo salvo
+                draft: draft === 'true',  // Converte string para booleano
+                published: published === 'true',  // Converte string para booleano
                 category_id,
                 author_id,
             });
--- src/routers.ts
+++ src/routers.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import multer from 'multer';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
@@ -8,9 +9,11 @@ import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryControllers } from './controllers/category/CreateCategoryControllers';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 import {CreatePostController} from './controllers/post/CreatePostController'
-
+import uploadConfig from './config/multer'
 
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 // -- ROTAS USERS --
 router.post('/users', ensureAdmin, new CreateUserController().handle)
@@ -24,7 +27,7 @@ router.get('/category',isAuthenticated, new ListCategoryController().handle)
 
 // -- ROTAS POSTS --
 
-router.post('/post',isAuthenticated, new CreatePostController().handle)
+router.post('/post',isAuthenticated, upload.single('file'), new CreatePostController().handle)
 
 
 
