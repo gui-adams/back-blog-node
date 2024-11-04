671ea5627017f1f9d3cb504f10778d0b8771db00
gui-adams <guilherme.adams@live.com>
2024-11-04 14:40:55 -0300

    passado

--- src/createAdmin.ts
+++ src/createAdmin.ts
@@ -1,30 +1,43 @@
-// createAdmin.ts
+// createUsers.ts
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
-async function createAdmin() {
-  // Criptografa a senha "windows99"
-  const hashedPassword = await bcrypt.hash('windows99', 10);
+async function createUsers() {
+  // Criptografa as senhas
+  const passwordGuilherme = await bcrypt.hash('windows99', 10);
+  const passwordJessica = await bcrypt.hash('windows99', 10);
 
   try {
-    // Cria o usuário administrador
-    const admin = await prisma.user.create({
+    // Cria o usuário Guilherme
+    const guilherme = await prisma.user.create({
       data: {
-        name: 'Admin',
+        name: 'Guilherme Adams',
         email: 'guilherme.adams@live.com',
-        password: hashedPassword,
+        password: passwordGuilherme,
         role: 'admin',
       },
     });
 
-    console.log('Usuário administrador criado:', admin);
+    console.log('Usuário Guilherme criado:', guilherme);
+
+    // Cria o usuário Jessica
+    const jessica = await prisma.user.create({
+      data: {
+        name: 'Jessica Camargo',
+        email: 'jessica.camargo@simpleway.tech',
+        password: passwordJessica,
+        role: 'admin',
+      },
+    });
+
+    console.log('Usuário Jessica criado:', jessica);
   } catch (error) {
-    console.error('Erro ao criar o usuário administrador:', error);
+    console.error('Erro ao criar os usuários:', error);
   } finally {
     await prisma.$disconnect();
   }
 }
 
-createAdmin();
+createUsers();
